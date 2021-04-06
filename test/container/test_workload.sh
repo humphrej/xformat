6422973478729ecfb767a90a2aade5ca4f16d0d5
Jasper Humphrey <jasper@specialprojectslab.com>
2021-04-06 19:06:23 +0200

    Added ignore_blank_lines to the test diffs

--- test/container/test_workload.sh
+++ test/container/test_workload.sh
@@ -4,7 +4,7 @@ echo Running xformat...
 /usr/bin/java -cp $(</app/xformat/image/xformat_image.classpath) xformat.XFormatter /testdata/unformatted
 
 echo Diff is...
-diff -r /testdata/unformatted /testdata/formatted
+diff -Br /testdata/unformatted /testdata/formatted
 ret=$?
 
 if [ "$ret" -eq 0 ]; then
---
+++ test/data/unformatted/clojure/transform.clj
@@ -0,0 +1,10 @@
+(defn transform* [person] (-> person
+      (assoc :hair-color :gray)
+      (update :age inc)))
+
+(defn transform* [person]
+  (-> person
+      (assoc :hair-color :gray)
+      (update :age inc)))
+
+
